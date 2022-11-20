fe1ff3343635e5171c72568f2e5dc057af43a5a8
zaminalirustemov <2g17rck@code.edu.az>
2022-11-20 20:42:57 +0400

    Homework

---
+++ Task3/ConsoleApp3/ConsoleApp3/Car.cs
@@ -0,0 +1,41 @@
+﻿namespace ConsoleApp3
+{
+    internal class Car
+    {
+        public Car(string brand, string model, double currentfuel, double fuelfor1km, double millage)
+        {
+            this.Brand = brand;
+            this.Model = model;
+            this.CurrentFuel=currentfuel;
+            this.FuelFor1Km=fuelfor1km;
+            this.Millage=millage;
+        }
+        public string Brand;
+        public string Model;
+        public double CurrentFuel;
+        public double FuelFor1Km;
+        public double Millage;
+
+        public double Drive(double distance)
+        {
+
+            if (distance <= CurrentFuel / FuelFor1Km)
+            {
+                CurrentFuel -= (distance * FuelFor1Km);
+                Millage += distance;
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("This distance can be traveled.");
+                Console.WriteLine($"---Remaining fuel {CurrentFuel} liter.");
+                Console.WriteLine($"---The distance the car has traveled so far {Millage} km.");
+
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("There is not enough fuel.");
+            }
+
+            return CurrentFuel;
+        }
+    }
+}
---
+++ Task3/ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Transactions;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car("Lada", "2107", 27, 0.092, 56000);
+
+            while (car.CurrentFuel >= 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.Write("Enter the distance to be traveled (in km):");
+                double distance = Convert.ToDouble(Console.ReadLine());
+                car.Drive(distance);
+            }
+
+        }
+    }
+}
